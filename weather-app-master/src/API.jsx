35b074299d06f4a77fc1bf4dc5826c137b233206
Shashanks0616 <shashanksingh0616@gmail.com>
2024-02-13 11:47:19 +0530

    Complete the Weather APP

---
+++ weather-app-master/src/API.jsx
@@ -0,0 +1,12 @@
+export const geoAPIOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "c2301b1f3bmsha4b48944d896a95p1b0f87jsn662cfd94e080",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "8b5f5cb93aa3972fdaa3ffd77553abb5";
